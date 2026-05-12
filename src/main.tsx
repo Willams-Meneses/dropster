02f2e81c2989846cb0b653139d4e53125e913d69
Willams Meneses <menesestapia@gmail.com>
2026-05-12 15:08:40 -0300

    Added basic login with layout to test theme configuration and update main.tsx to use theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App';
+import theme from './theme';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
---
+++ src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { Button, Typography } from "@mui/material"
+
+const Login = () => {
+  return (
+    <div>
+      This text doest have poppins
+
+      <Typography variant="h1" >This text has poppins</Typography>
+
+      <Button variant="contained" color="primary"> Primary burtton</Button>
+
+    </div>
+  )
+}
+
+export default Login
