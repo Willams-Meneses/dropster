ae73c336517b979cc1d07602b0b4c928dd4db6d3
Willams Meneses <menesestapia@gmail.com>
2026-05-12 15:01:12 -0300

    added shortcuts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'  // ← Agrega esta línea
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {              // ← Agrega todo este bloque
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+    }
+  }
 })
