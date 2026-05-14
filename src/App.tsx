7ed3bbdbb545b24c55e3ecc090f53c53d9e91126
Willams Meneses <menesestapia@gmail.com>
2026-05-14 16:40:36 -0300

    added new routes for login, register and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AuthLayout from '@/layouts/AuthLayout';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import { useAuthStore } from '@/store/auth.store';
-import RegisterRoleSelection from './pages/RegisterRoleSelection';
+import RegisterRoleSelection from './pages/RoleSelection';
 import ProtectedRoute from './components/ui/ProtectedRoute';
 
 // Placeholder para rutas protegidas — reemplazá con tus páginas reales
@@ -32,12 +32,16 @@ function App() {
       <Route path="/select-role" element={<RegisterRoleSelection />} />
 
       {/* Rutas protegidas */}
+      {/* TODO: El protectedRoute es un layout de rutas que protege las rutas hijas y se encarga de redireccionar al dashboard si cumple con los
+      requerimientos de este layout, sino cumple redirige al path correspondiente. Por lo tanto cuando api da success por ejemplo register
+      ya no se redirecciona al dashboard de eso se encarga protectedRoute */}
       <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
 
       {/* Fallback */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
