2ee6cd3144fc05d5534b443c58cfe6112ca35c73
Willams Meneses <menesestapia@gmail.com>
2026-05-15 17:08:55 -0300

    Added icons components for sidebar

---
+++ src/components/icons/OrdersIcon.tsx
@@ -0,0 +1,48 @@
+export function OrdersIcon({ className = "w-4 h-4" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      className={className}
+    >
+      <path
+        d="M7 6.5625V9.625"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.0625 4.8125L7 1.3125L3.9375 4.8125"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.88828 6.5625L9.58203 9.625"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.11328 6.5625L4.41953 9.625"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.3125 4.8125H12.6875L11.8634 10.9955C11.8493 11.1005 11.7976 11.1969 11.7178 11.2667C11.6381 11.3365 11.5357 11.375 11.4297 11.375H2.57031C2.46432 11.375 2.36192 11.3365 2.28216 11.2667C2.20239 11.1969 2.15069 11.1005 2.13664 10.9955L1.3125 4.8125Z"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/RefundIcon.tsx
@@ -0,0 +1,27 @@
+export function RefundIcon({ className = "w-4 h-4" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      className={className}
+    >
+      <path
+        d="M4.375 7.4375L1.75 4.8125L4.375 2.1875"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.375 10.9375H9.1875C9.99973 10.9375 10.7787 10.6148 11.353 10.0405C11.9273 9.46618 12.25 8.68723 12.25 7.875C12.25 7.06277 11.9273 6.28382 11.353 5.70949C10.7787 5.13516 9.99973 4.8125 9.1875 4.8125L1.75 4.8125"
+        stroke="currentColor"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
