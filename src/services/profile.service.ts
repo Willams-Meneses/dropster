3b97cf43f04acf43687cb25ddfd3e67d75e2431f
Willams Meneses <menesestapia@gmail.com>
2026-08-11 16:35:14 -0300

    Added new endpoint to edit address for provider users

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,9 +1,15 @@
 import api from '@/lib/axios';
+import type { AddressFormValues } from '@/schemas/address.schema';
 import type { ProfileResponse, UpdateProfilePayload } from '@/types/profile.types';
 
 export const profileService = {
   updateMyInfo: async (payload: UpdateProfilePayload): Promise<ProfileResponse> => {
     const { data } = await api.put<ProfileResponse>('/auth/me/profile', payload);
+    return data;
+  },
+
+  updateAddress: async (payload: AddressFormValues): Promise<ProfileResponse> => {
+    const { data } = await api.put<ProfileResponse>('/auth/me/address', payload);
     return data;
   },
 };
